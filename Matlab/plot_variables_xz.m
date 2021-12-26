7931220e7b1119a96c068a69c412ad5a5ca54d55
David Marsico <69769685+dmarsico1@users.noreply.github.com>
2021-12-26 15:30:26 -0800

    Add files via upload

---
+++ Matlab/plot_variables_xz.m
@@ -0,0 +1,78 @@
+function plot_variables_xz(filename)
+
+ncid = netcdf.open(filename,'NC_NOWRITE');
+
+[dimname0,dimlen0] = netcdf.inqDim(ncid,0);
+[dimname1,dimlen1] = netcdf.inqDim(ncid,1);
+[dimname1,dimlen2] = netcdf.inqDim(ncid,2);
+
+vecStrVariableList=["w","b","u","v"];
+
+matVar=zeros(4,dimlen2,dimlen0,dimlen1);
+
+dL=10.0;
+a=1.0;
+Nx=dimlen0;
+Ny=dimlen1;
+hx=2*dL/Nx;
+hy=2*dL/Ny;
+hz=1.0/dimlen2;
+
+y_slice=dimlen1/2;
+
+ub=(Nx/2)+2*a/hx
+lb=(Nx/2)-2*a/hy
+
+xt=-dL+hx/2:hx:dL-hx/2;
+yt=-dL+hy/2:hy:dL-hy/2;
+zt=hz/2:hz:1-hz/2;
+
+
+matVar(1,:,:,:)=ncread(filename,'w');
+matVar(2,:,:,:)=ncread(filename,'b');
+matVar(3,:,:,:)=ncread(filename,'u');
+matVar(4,:,:,:)=ncread(filename,'v');
+
+%get plot bounds for u,v,w
+
+bound_vec=zeros(1,4);
+
+for i=1:4
+    
+    maxVar=max(max(squeeze(matVar(i,:,:,y_slice))));
+    minVar=min(min(squeeze(matVar(i,:,:,y_slice))));
+    bound_vec(i)=min([abs(maxVar),abs(minVar)]);
+    
+end
+
+
+h_vec=zeros(1,4);
+
+for i=1:4
+   
+    h_vec(i)=2*bound_vec(i)/10;
+    
+end
+
+figure(1)
+for i=1:4
+    
+    subplot(2,2,i)
+    if(i==2)
+        contourf(xt(lb:ub),zt,(squeeze(matVar(i,:,lb:ub,dimlen1/2))),10)
+
+    else
+        contourf(xt(lb:ub),zt,(squeeze(matVar(i,:,lb:ub,dimlen1/2))),-bound_vec(i):h_vec(i):bound_vec(i))
+    end
+    colormap(redblueTecplot)
+    colorbar
+    title(vecStrVariableList(i),'interpreter','latex')
+    xlabel('x')
+    ylabel('z')
+    hold on
+    
+end
+
+hold off
+
+end
